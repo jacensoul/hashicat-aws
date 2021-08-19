686dd742c7a18e6349f70697fe9f39a7987c92b2
Kevin Porter <kporter@unfi.com>
2021-08-19 16:39:16 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
 module "s3-bucket" {
-  source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "2.7.0"
+  source  = "app.terraform.io/unfi-kporter/s3-bucket/aws"
+  version = "2.2.0"
   bucket_prefix = "kevin porter"
 }
